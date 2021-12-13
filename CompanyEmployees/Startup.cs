4f251134c55c888097e3c9d784f891205b2853dd
Neal Robben <neal.robben@gmail.com>
2021-12-13 13:47:20 +0100

    Handling GET requests

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -30,6 +30,8 @@ namespace CompanyEmployees
             services.ConfigureSqlContext(Configuration);
             services.ConfigureRepositoryManager();
 
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddControllers();
         }
 
--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Contracts;
 using Entities;
 using Entities.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Repository
 {
@@ -8,6 +10,11 @@ namespace Repository
     {
         public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
         {
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+            return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
         }
     }
 }
