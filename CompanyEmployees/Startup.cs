dc16a87d2d8bcb26718e3b4d130f1fe9b1d935f9
Neal Robben <neal.robben@gmail.com>
2021-12-13 14:42:05 +0100

    Content negotiation

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -33,7 +33,12 @@ namespace CompanyEmployees
 
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddControllers();
+            services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters()
+            .AddCustomCSVFormatter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
